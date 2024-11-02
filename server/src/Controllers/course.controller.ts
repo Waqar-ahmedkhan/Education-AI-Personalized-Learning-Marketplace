1cea7be475f2342d7e1d13d58b42c5d9217d3bb0
Waqar ahmed <waqarahmed44870@gmail.com>
2024-11-02 21:50:17 +0500

    uploadcourse

---
+++ server/src/Controllers/course.controller.ts
@@ -0,0 +1,28 @@
+import { NextFunction, Request, Response } from "express";
+import { CatchAsyncError } from "../middlewares/CatchAsyncError";
+import { AppError } from "../utils/AppError";
+import  Cloudinary from "cloudinary";
+
+export const uploadCourse = CatchAsyncError( async (req: Request, res: Response, next: NextFunction)=> {
+   try {
+
+       const data = req.body;
+       const Thumbnail = data.thumbnail;
+       if(Thumbnail){
+       const myCloud = await Cloudinary.v2.uploader.upload(Thumbnail, {
+          folder: "courses",
+        })
+
+       data.thumbnail = {
+          public_id: myCloud.public_id,
+          url: myCloud.secure_url,
+         }
+       }
+
+     
+   }
+   catch(err){
+    console.log(err);
+    next( new AppError("course are not uploaded thair are some error happening ", 400))
+   }
+})
